2288a10f6fca1bd8e13cbabf0983e8e5be17d5b3
Stephen Dillon <yodareloaded@gmail.com>
2021-04-01 11:41:52 -0400

    start.spring.io generated project

---
+++ src/main/java/eclipsepluginissue/buildship1076/Buildship1076Application.java
@@ -0,0 +1,13 @@
+package eclipsepluginissue.buildship1076;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Buildship1076Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Buildship1076Application.class, args);
+	}
+
+}
---
+++ src/test/java/eclipsepluginissue/buildship1076/Buildship1076ApplicationTests.java
@@ -0,0 +1,13 @@
+package eclipsepluginissue.buildship1076;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Buildship1076ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
